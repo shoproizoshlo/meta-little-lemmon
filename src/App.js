4afafc375b6c2cb25b29eb38c1176d9360b7b4dd
shoproizoshlo <iamsusunny@gmail.com>
2023-11-03 16:37:18 +0100

    created structure of the project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
