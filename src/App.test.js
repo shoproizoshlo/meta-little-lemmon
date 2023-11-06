7edcfd8d61eb422f1586662617a0a0f369d00bca
shoproizoshlo <iamsusunny@gmail.com>
2023-11-06 16:43:56 +0100

    tested updateTimes

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import BookingForm from "./components/BookingPage/BookingForm";
 import { initializeTimes } from "./components/Main";
+import { updateTimes } from "./components/Main";
 
 test("Renders the BookingForm heading", () => {
   render(<BookingForm />);
@@ -9,11 +10,15 @@ test("Renders the BookingForm heading", () => {
   expect(headingElement).toBeInTheDocument();
 });
 
-const expect = require("expect");
-
 test("initializeTimes should return an array of times", () => {
   const times = initializeTimes();
   expect(times).toBeDefined();
   expect(times).toBeInstanceOf(Array);
   expect(times.length).toBeGreaterThan(0);
+});
+
+test("updateTimes should return the same value as provided in the state", () => {
+  const currentState = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  expect(updateTimes(currentState)).toEqual(currentState);
 });
