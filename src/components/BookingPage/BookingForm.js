eaccde7eb8a94898457814c9501c7977bfb99e3b
shoproizoshlo <iamsusunny@gmail.com>
2023-11-08 12:58:00 +0100

    form validation using formik

--- src/components/BookingPage/BookingForm.js
+++ src/components/BookingPage/BookingForm.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import { useFormik, Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function BookingForm(props) {
   const [guests, setGuests] = useState(2);
   const [occasion, setOccasion] = useState("");
   const [date, setDate] = useState("");
   const [finalTime, setFinalTime] = useState(
-    props.availableTimes.map((times) => (
-      <option key={times}>{times}</option>
-    ))
+    props.availableTimes.map((times) => <option key={times}>{times}</option>)
   );
 
   function handleDateChange(e) {
@@ -20,20 +20,32 @@ export default function BookingForm(props) {
     props.updateTimes(date);
 
     setFinalTime(
-      props.availableTimes.map((times) => (
-        <option key={times}>{times}</option>
-      ))
+      props.availableTimes.map((times) => <option key={times}>{times}</option>)
     );
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const formik = useFormik({
+    initialValues: {
+      guests: "",
+      occasion: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      guests: Yup.number()
+        .max(10, "Maximum be 10 guests or email us if you want more guests")
+        .required("Required"),
+      occasion: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
       <p>This is BookingForm</p>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={formik.handleSubmit}
         style={{ display: "grid", maxWidth: 200, gap: 20 }}
       >
         <label htmlFor="res-date">Choose date</label>
@@ -48,28 +60,44 @@ export default function BookingForm(props) {
         <select id="res-time" name="res-time">
           {finalTime}
         </select>
+
         <label htmlFor="guests">Number of guests</label>
         <input
           type="number"
           placeholder={1}
-          min={1}
-          max={10}
           id="guests"
           name="guests"
-          value={guests}
-          onChange={(e) => setGuests(e.target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.guests}
         />
+        {formik.touched.guests && formik.errors.guests ? (
+          <div>{formik.errors.guests}</div>
+        ) : null}
+
         <label htmlFor="occasion">Occasion</label>
         <select
           id="occasion"
           name="occasion"
-          value={occasion}
-          onChange={(e) => setOccasion(e.target.value)}
+          onChange={formik.handleChange}
+          value={formik.values.occasion}
         >
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
-        {/* <input type="submit" defaultValue="Make Your reservation" /> */}
+        {formik.errors.occasion ? <div>{formik.errors.occasion}</div> : null}
+
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+
         <button type="submit">Book Now</button>
       </form>
     </>
