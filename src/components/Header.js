a84305730332307c9b2cbba3ba6ccb3f73b4b3be
shoproizoshlo <iamsusunny@gmail.com>
2023-11-10 11:27:29 +0100

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
 import "./Header.css";
+import Logo from "../images/Logo.svg";
 
 export default function Header() {
   return (
     <>
-      <header className="container d-md-flex justify-content-center">
-        <img alt="logo" />
+      <header className="container d-md-flex justify-content-center align-items-center">
+        <img src={Logo} alt="logo" />
         <Nav />
       </header>
     </>
