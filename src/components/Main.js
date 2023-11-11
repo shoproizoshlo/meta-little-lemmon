9c293ab2e271f6107e6dcc115c20dc69e7b90525
shoproizoshlo <iamsusunny@gmail.com>
2023-11-11 08:57:54 +0100

    created menu nemu

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import AboutPage from "./AboutPage/AboutPage";
 import BookingPage from "./BookingPage/BookingPage";
 import MenuPage from "./MenuPage/MenuPage";
 import ThankYouPage from "./PageComponents/ThankYouPage";
+import SpecialsMenu from "./MenuPage/SpecialsMenu";
 
 export default function Main() {
   return (
@@ -16,6 +17,22 @@ export default function Main() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/booking" element={<BookingPage />} />
           <Route path="/menu" element={<MenuPage />} />
+          <Route
+            path="/menu/specials"
+            element={<SpecialsMenu special="special" />}
+          />
+          <Route
+            path="/menu/entrees"
+            element={<SpecialsMenu category="Entrees" />}
+          />
+          <Route
+            path="/menu/appetizers"
+            element={<SpecialsMenu category="Appetizers" />}
+          />
+          <Route
+            path="/menu/desserts"
+            element={<SpecialsMenu category="Desserts" />}
+          />
           <Route path="/booking-submit" element={<ThankYouPage />} />
         </Routes>
       </main>
--- src/components/MenuPage/Menu.js
+++ src/components/MenuPage/Menu.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SpecialsMenu from "./SpecialsMenu";
 
 export default function Menu() {
   return (
     <>
+      <ul className="d-md-flex">
+        <li>
+          <Link to="/menu/specials">Specials</Link>
+        </li>
+        <li>
+          <Link to="/menu/entrees">Entrees</Link>
+        </li>
+        <li>
+          <Link to="/menu/appetizers">Appetizers</Link>
+        </li>
+        <li>
+          <Link to="/menu/desserts">Desserts</Link>
+        </li>
+      </ul>
       <p>Specials</p>
       <SpecialsMenu special="special" />
       <p>Entrees</p>
