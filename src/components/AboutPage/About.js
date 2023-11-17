62eda8669a105e5141109eaf72b689d0c5ff3672
shoproizoshlo <iamsusunny@gmail.com>
2023-11-17 16:38:04 +0100

    fixed about section

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -5,7 +5,7 @@ export default function About(props) {
   return (
     <>
       <div className="about container-custom d-md-flex justify-content-between align-items-center">
-        <div>
+        <div className="ms-1">
           <h2>{props.h2}</h2>
           <p>{props.description}</p>
         </div>
