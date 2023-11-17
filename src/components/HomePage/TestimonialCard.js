2182b2ff7773c8cba209873e9ac1f0286ec0952f
shoproizoshlo <iamsusunny@gmail.com>
2023-11-17 12:37:13 +0100

    fixed positions testimonials and photos, added icons

--- src/components/HomePage/TestimonialCard.js
+++ src/components/HomePage/TestimonialCard.js
@@ -1,18 +1,39 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLemon } from "@fortawesome/free-solid-svg-icons";
+import "./TestimonialCard.css";
 
 const TestimonialCard = ({ testimonialItem }) => {
+  const rating = parseInt(testimonialItem["rating"], 10) || 0;
+
+  // Array to store the repeated icons
+  const ratingIcons = Array.from({ length: rating }, (_, index) => (
+    <FontAwesomeIcon
+      key={index}
+      icon={faLemon}
+      size="xl"
+      className="lemon-icon"
+    />
+  ));
+
   return (
     <>
       <article className="d-flex flex-column m-3">
         <div
-          className="d-flex justify-content-md-start justify-content-sm-center
+          className="d-flex justify-content-center justify-content-md-start align-items-center
 "
         >
-          <img src={testimonialItem["image"]} alt="user" />
-          <p className="highlight ms-4">{testimonialItem["name"]}</p>
+          <img
+            src={testimonialItem["image"]}
+            alt="user"
+            className="user-photo"
+          />
+          <p className="highlight ms-4  mb-0">{testimonialItem["name"]}</p>
         </div>
         <div>
-          <p className="highlight">{testimonialItem["rating"]}</p>
+          <p className="highlight mt-3">
+            {testimonialItem["rating"]} {ratingIcons}
+          </p>
         </div>
         <p>{testimonialItem["review"]}</p>
       </article>
