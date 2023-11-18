6dfd7efb699c423153120c076d96d7455c210134
shoproizoshlo <iamsusunny@gmail.com>
2023-11-18 11:46:24 +0100

    styled menu page

--- src/components/MenuPage/MenuPage.js
+++ src/components/MenuPage/MenuPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import TopOfThePage from "../PageComponents/TopOfThePage";
 import Menu from "./Menu";
+import Chefs from "../../assets/Mario-and-Adrian-B.jpg";
+import "./MenuPage.css";
 
 export default function MenuPage() {
   return (
     <>
-      <div>
-        <TopOfThePage h1="Menu" h2="Find our amazing dishes" topPhoto="/" />
+      <div className="menu-top">
+        <TopOfThePage
+          h1="Menu"
+          text="Find our amazing dishes"
+          topPhoto={Chefs}
+        />
       </div>
       <div className="container-custom">
         <Menu />
