cfb54703d604c6bd05561a062e1d4e74a9fead1f
shoproizoshlo <iamsusunny@gmail.com>
2023-11-20 15:00:05 +0100

    added menu navigation

--- src/components/HomePage/Specials.js
+++ src/components/HomePage/Specials.js
@@ -10,7 +10,7 @@ export default function Specials() {
         <section>
           <div className="specials d-flex align-items-center justify-content-between mb-5">
             <h2>This week specials</h2>
-            <CallToAction url="menu" callToAction="Online Menu" />
+            <CallToAction url="/menu" callToAction="Online Menu" />
           </div>
           <SpecialsMenu displayCount={3} special="special" />
         </section>
