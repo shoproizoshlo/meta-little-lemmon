fd2e57be57e42413e23ebea7af1152360765bc33
shoproizoshlo <iamsusunny@gmail.com>
2023-11-27 17:27:42 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { HashLink } from "react-router-hash-link";
 import Nav from "./Nav";
 import Logo from "../assets/Logo.svg";
 import "./Header.css";
