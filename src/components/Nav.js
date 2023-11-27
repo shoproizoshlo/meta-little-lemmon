a40d812507fb61c63ae81dc84eee37c22bdf048f
shoproizoshlo <iamsusunny@gmail.com>
2023-11-27 15:12:13 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,22 +20,22 @@ export default function Nav(props) {
         </div>
         <ul className="nav-menu" id="menu">
           <li className="q-line" data-target="menu-1">
-            <HashLink smooth to={"/#homepage"} onClick={menuClose}>
+            <HashLink smooth to={"/#menu-icon"} onClick={menuClose}>
               Home
             </HashLink>
           </li>
           <li className="q-line" data-target="menu-2">
-            <HashLink smooth to={"/about/#aboutpage"} onClick={menuClose}>
+            <HashLink smooth to={"/about#menu-icon"} onClick={menuClose}>
               About
             </HashLink>
           </li>
           <li className="q-line" data-target="menu-3">
-            <HashLink smooth to={"/booking/#maintop"} onClick={menuClose}>
+            <HashLink smooth to={"/booking#menu-icon"} onClick={menuClose}>
               Online Reservation
             </HashLink>
           </li>
           <li className="q-line" data-target="menu-4">
-            <HashLink smooth to={"/menu/#maintop"} onClick={menuClose}>
+            <HashLink smooth to={"/menu#menu-icon"} onClick={menuClose}>
               Menu
             </HashLink>
           </li>
