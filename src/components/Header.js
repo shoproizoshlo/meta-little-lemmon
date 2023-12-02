9fff37715683ea29cb24ffefd9fd09e2da6dc668
shoproizoshlo <iamsusunny@gmail.com>
2023-12-02 11:26:41 +0100

    added nav to homepage when clicked on header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import the useNavigate hook
+
 import Nav from "./Nav";
 import Logo from "../assets/Logo.svg";
 import "./Header.css";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+
+  // Function to navigate to the home page
+  const goToHomePage = () => {
+    navigate("/"); // Update the path based on your home page route
+  };
 
   function toggleMenu() {
     setIsMenuOpen((prevState) => !prevState);
@@ -25,7 +33,7 @@ export default function Header() {
     <>
       <div className="overlay" onClick={toggleMenu}></div>
       <header className="d-flex justify-content-between justify-content-md-center align-items-center fixed-top pt-2 pe-4 pb-2 ps-4">
-        <img src={Logo} alt="logo" />
+        <img src={Logo} alt="logo" onClick={goToHomePage} />
         <Nav isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       </header>
     </>
